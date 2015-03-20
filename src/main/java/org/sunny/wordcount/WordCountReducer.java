730d0734e889e349c71242d157f5d502a40c76e9
Sunny Shah <sunny.cpp@gmail.com>
2015-03-20 17:10:49 +0530

    Added conf

--- src/main/java/org/sunny/wordcount/WordCountReducer.java
+++ src/main/java/org/sunny/wordcount/WordCountReducer.java
@@ -2,11 +2,20 @@ package org.sunny.wordcount;
 
 import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class WordCountReducer extends
 		Reducer<Text, IntWritable, Text, IntWritable> {
+	int threasoldForCount = 10;
+	@Override
+	protected void setup(org.apache.hadoop.mapreduce.Reducer<Text,IntWritable,Text,IntWritable>.Context context) throws IOException ,InterruptedException {
+		super.setup(context);
+		Configuration conf =  context.getConfiguration();
+		threasoldForCount = conf.getInt("threasold-for-count", 10);
+	};
+	
 	@Override
 	protected void reduce(Text key, Iterable<IntWritable> values,
 			Reducer<Text, IntWritable, Text, IntWritable>.Context context)
@@ -14,7 +23,8 @@ public class WordCountReducer extends
 		int count = 0;
 		for(IntWritable value:values) {
 			count += value.get();
-		}		
-		context.write(key,new IntWritable(count));
+		}
+		if(count > threasoldForCount)
+			context.write(key,new IntWritable(count));
 	}
 }
