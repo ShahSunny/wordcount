55ed77969b9a698f506875e5bb02b883b7ecf14b
Sunny Shah <sunny.cpp@gmail.com>
2015-03-20 18:10:05 +0530

    added prints to test configuration

--- src/main/java/org/sunny/wordcount/WordCountReducer.java
+++ src/main/java/org/sunny/wordcount/WordCountReducer.java
@@ -3,6 +3,9 @@ package org.sunny.wordcount;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -11,9 +14,15 @@ public class WordCountReducer extends
 	int threasoldForCount = 10;
 	@Override
 	protected void setup(org.apache.hadoop.mapreduce.Reducer<Text,IntWritable,Text,IntWritable>.Context context) throws IOException ,InterruptedException {
+		System.out.println("*****************************************************************");
+		System.out.println("In setup");
+		System.out.println("*****************************************************************");
+		
 		super.setup(context);
 		Configuration conf =  context.getConfiguration();
-		threasoldForCount = conf.getInt("threasold-for-count", 10);
+		threasoldForCount = conf.getInt("threasold-for-count", 10);		
+		FileSystem.create(FileSystem.get(conf), new Path("/tmp/threasoldForCount_" + threasoldForCount), FsPermission.getDefault());
+		System.out.println("threasold-for-count = " + threasoldForCount);
 	};
 	
 	@Override
