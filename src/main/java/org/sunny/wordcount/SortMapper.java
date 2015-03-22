e88f6a15154a9efabb48b5c9ede677e2067b5532
Sunny Shah <sunny.cpp@gmail.com>
2015-03-22 07:55:53 +0530

    Removed job to count for temp testing, fixed issue of setting key type as Text for SortMapper

--- src/main/java/org/sunny/wordcount/SortMapper.java
+++ src/main/java/org/sunny/wordcount/SortMapper.java
@@ -6,11 +6,12 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class SortMapper extends Mapper<Text, IntWritable, IntWritable, Text> {
+public class SortMapper extends Mapper<Text, Text, IntWritable, Text> {
 	@Override
-	protected void map(Text key, IntWritable value,
-			Mapper<Text, IntWritable, IntWritable, Text>.Context context)
+	protected void map(Text key, Text value,
+			Mapper<Text, Text, IntWritable, Text>.Context context)
 			throws IOException, InterruptedException {
-		context.write(value, key);
+		int count = Integer.parseInt(value.toString());
+		context.write(new IntWritable(count), key);
 	}
 }
