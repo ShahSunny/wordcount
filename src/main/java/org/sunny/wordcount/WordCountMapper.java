7593d8acd008e4bd1bafa45ab1707c29b068c570
Sunny Shah <sunny.cpp@gmail.com>
2015-03-23 22:39:25 +0530

    Added counter

--- src/main/java/org/sunny/wordcount/WordCountMapper.java
+++ src/main/java/org/sunny/wordcount/WordCountMapper.java
@@ -10,6 +10,11 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class WordCountMapper extends
 		Mapper<Text, NullWritable, Text, IntWritable> {
+	 
+	enum CodeLine {
+		    ZeroWords,
+		    NonZeroWords
+	 }
 	public static String[] splitLines(String line) {
 		return line.split("((?!_)\\p{Punct}|\\s)+",0);
 	}
@@ -18,10 +23,19 @@ public class WordCountMapper extends
 	      throws IOException, InterruptedException {		
 	    String line = key.toString();
 	    String[] words = splitLines(line);
+	    int wordCounter = 0;
 	    for(String word:words) {
 	    	if(word.length() > 1) {
+	    		++wordCounter;
 	    		context.write(new Text(word), new IntWritable(1));
 	    	}
+	    }
+	    if(wordCounter > 0) {
+	    	context.getCounter(CodeLine.NonZeroWords).increment(1);
+	    	context.getCounter("Code_Line", "Non_Zero_Words").increment(1);
+	    } else {
+	    	context.getCounter(CodeLine.ZeroWords).increment(1);
+	    	context.getCounter("Code_Line", "Zero_Words").increment(1);
 	    }
 	  }
 }
